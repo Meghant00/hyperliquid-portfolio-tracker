bb4541045a68932534539cbddf2f2909cc146548
Meghant00 <dangolmeghant0@gmail.com>
2026-05-25 13:20:32 +0545

    :package: : add wagmi

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { WagmiProvider } from "wagmi";
+import { config } from "./config/config";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
 function App() {
   return (
     <>
-      <h1 className="tw:text-3xl tw:text-red-600"> Hello World</h1>
+      <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <h1 className="tw:text-3xl tw:text-red-600"> Hello World</h1>
+        </QueryClientProvider>
+      </WagmiProvider>
     </>
   );
 }
