b905a6e7327d11bdbdf8e69b53a7a718df2f96b6
Meghant00 <dangolmeghant0@gmail.com>
2026-06-08 16:33:16 +0545

    :sparkles: : setup redux

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./assets/css/index.css";
 import "./assets/css/main.css";
 import App from "./App.tsx";
+import { Provider } from "react-redux";
+import { store } from "./store.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 );
