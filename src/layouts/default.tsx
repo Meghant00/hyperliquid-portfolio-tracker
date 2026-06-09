54f7a6ec626b5c7463a053ffd53ca9c0a48c5c10
Meghant00 <dangolmeghant0@gmail.com>
2026-06-09 16:51:38 +0545

    :sparkles: : use useUserFills hook in default layout

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,7 +1,68 @@
 import { Outlet } from "react-router";
 import Navbar from "../components/Navbar/Navbar";
+import { useConnection } from "wagmi";
+import { useUserFills } from "../hooks/hyperliquid/useUserFillsFromSocket";
+import { useUserClearHouseState } from "../hooks/hyperliquid/useUserClearingHouseState";
+import { useEffect } from "react";
+import { getUserFills } from "../services/user";
+import { useAppDispatch, useAppSelector } from "../hooks/state";
+import type { UserFillsWsEvent } from "@nktkas/hyperliquid";
+import { setUserFills } from "../slices/userFillsSlice";
 
 const DefaultLayout = () => {
+  const userFills = useAppSelector((state) => state.userFills.userFills);
+
+  const dispatch = useAppDispatch();
+
+  const { address } = useConnection();
+
+  const { userFills: userFillsFromSocket, unsubscribeUserFills } = useUserFills(
+    { address },
+  );
+  const { unsubscribeToUserClearingHouseState } = useUserClearHouseState({
+    address,
+  });
+
+  useEffect(() => {
+    const abortController = new AbortController();
+
+    if (!address) {
+    } else {
+      unsubscribeUserFills();
+      unsubscribeToUserClearingHouseState();
+      fetchAndSetUserFills(abortController.signal);
+    }
+
+    return () => {
+      abortController.abort;
+    };
+  }, [address]);
+
+  useEffect(() => {
+    setUserFillsFromSocket(userFillsFromSocket);
+
+    return () => {};
+  }, [userFillsFromSocket]);
+
+  const setUserFillsFromSocket = (userFillsEvent: UserFillsWsEvent | null) => {
+    if (userFillsEvent) {
+      if (!userFillsEvent.isSnapshot) {
+        dispatch(setUserFills([...userFillsEvent.fills, ...userFills]));
+      }
+    }
+  };
+
+  const fetchAndSetUserFills = async (signal: AbortSignal) => {
+    if (address) {
+      const userFillsRes = await getUserFills({
+        address: address,
+        signal: signal,
+      });
+
+      dispatch(setUserFills(userFillsRes));
+    }
+  };
+
   return (
     <div>
       <Navbar />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,18 @@
-import { useConnection } from "wagmi";
 import PerformanceOverview from "../components/Portfolio/PerformanceOverview";
-import { useUserFills } from "../hooks/hyperliquid/useUserFillsFromSocket";
 import { useEffect, useState } from "react";
-import type { UserFillsWsEvent } from "@nktkas/hyperliquid";
 import {
   calculateLargestLossAndAverageLoss,
   calculateLargestProfitAndAverageProfit,
   calculateTotalWinsAndLossesFromUserFills,
 } from "../utils/performanceOverview";
-import { getUserFills } from "../services/user";
 import BestAndWorst from "../components/Portfolio/BestAndWorst";
 import PortfolioOverview from "../components/Portfolio/Overview/Overview";
-import { useUserClearHouseState } from "../hooks/hyperliquid/useUserClearingHouseState";
-import { useAppDispatch, useAppSelector } from "../hooks/state";
-import { setUserFills } from "../slices/userFillsSlice";
-
+import { useAppSelector } from "../hooks/state";
+import { useConnection } from "wagmi";
 const Index = () => {
-  const { address } = useConnection();
-
   const userFills = useAppSelector((state) => state.userFills.userFills);
 
-  const dispatch = useAppDispatch();
-
-  const { userFills: userFillsFromSocket, unsubscribeUserFills } = useUserFills(
-    { address },
-  );
-  const { unsubscribeToUserClearingHouseState } = useUserClearHouseState({
-    address,
-  });
+  const { address } = useConnection();
 
   const [performanceOverview, setPerformanceOverview] = useState({
     totalTrades: 0,
@@ -44,29 +29,6 @@ const Index = () => {
   });
 
   useEffect(() => {
-    setUserFillsFromSocket(userFillsFromSocket);
-
-    return () => {};
-  }, [userFillsFromSocket]);
-
-  useEffect(() => {
-    const abortController = new AbortController();
-
-    if (!address) {
-      resetBestAndWorstTrades();
-      resetPerformanceOverview();
-    } else {
-      unsubscribeUserFills();
-      unsubscribeToUserClearingHouseState();
-      fetchAndSetUserFills(abortController.signal);
-    }
-
-    return () => {
-      abortController.abort;
-    };
-  }, [address]);
-
-  useEffect(() => {
     calculatePerformanceOverview();
     calculateBestAndWorstTrades();
 
@@ -76,27 +38,17 @@ const Index = () => {
     };
   }, [userFills]);
 
-  const setUserFillsFromSocket = (userFillsEvent: UserFillsWsEvent | null) => {
-    if (userFillsEvent) {
-      if (!userFillsEvent.isSnapshot) {
-        dispatch(setUserFills([...userFillsEvent.fills, ...userFills]));
-
-        calculatePerformanceOverview();
-        calculateBestAndWorstTrades();
-      }
+  useEffect(() => {
+    if (!address) {
+      resetPerformanceOverview();
+      resetBestAndWorstTrades();
     }
-  };
 
-  const fetchAndSetUserFills = async (signal: AbortSignal) => {
-    if (address) {
-      const userFillsRes = await getUserFills({
-        address: address,
-        signal: signal,
-      });
-
-      dispatch(setUserFills(userFillsRes));
-    }
-  };
+    return () => {
+      resetPerformanceOverview();
+      resetBestAndWorstTrades();
+    };
+  }, [address]);
 
   const calculatePerformanceOverview = () => {
     const { wins, losses } =
