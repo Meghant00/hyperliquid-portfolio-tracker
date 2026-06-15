38ade257f9fab33d44893245416c33e6bb4e0029
Meghant00 <dangolmeghant0@gmail.com>
2026-06-15 16:52:01 +0545

    :sparkles: : customize toast according to type

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -6,7 +6,7 @@ interface IToast {
   type: toastType;
   message: string;
   id: number;
-  additionalClass?: string;
+  isExiting?: boolean;
 }
 
 interface AddToastParameters {
@@ -43,7 +43,6 @@ const ToastProvider = ({ children }: { children: React.ReactNode }) => {
       message: message,
       type: type,
       id: toastId,
-      additionalClass: "pop-up",
     };
 
     setToasts((prevToasts) => [...prevToasts, toast]);
@@ -57,7 +56,7 @@ const ToastProvider = ({ children }: { children: React.ReactNode }) => {
     setToasts((prevToasts) =>
       prevToasts.map((toast) => {
         if (toast.id === toastId) {
-          toast.additionalClass = "pop-out";
+          toast.isExiting = true;
         }
 
         return toast;
@@ -76,7 +75,7 @@ const ToastProvider = ({ children }: { children: React.ReactNode }) => {
       value={{ toasts: toasts, addToast: addToast, removeToast: removeToast }}
     >
       {children}
-      <div className="tw:w-fit tw:absolute tw:top-0 tw:left-1/2 -translate-x-1/2 tw:py-2 tw:flex tw:flex-col tw:items-center tw:justify-center tw:gap-3">
+      <div className="tw:w-fit tw:absolute tw:top-0 tw:left-1/2 -translate-x-1/2 tw:py-2 tw:flex tw:flex-col tw:items-center tw:justify-center tw:gap-3 tw:transition-all tw:duration-150 tw:ease-linear">
         {toasts.map((toast) => {
           return <Toast toast={toast} key={toast.id} />;
         })}
@@ -86,11 +85,41 @@ const ToastProvider = ({ children }: { children: React.ReactNode }) => {
 };
 
 const Toast = ({ toast }: { toast: IToast }) => {
+  let toastIconClass = "bx bx-check-circle";
+  let toastIconColorClass = "tw:text-profit";
+
+  switch (toast.type) {
+    case "error":
+      toastIconClass = "bx bx-x-circle";
+      toastIconColorClass = "tw:text-loss";
+      break;
+    case "success":
+      toastIconClass = "bx bx-check-circle";
+      toastIconColorClass = "tw:text-profit";
+      break;
+    case "info":
+      toastIconClass = "bx bx-info-circle";
+      toastIconColorClass = "tw:text-blue-400";
+      break;
+    case "warning":
+      toastIconClass = "bx bx-alert-triangle";
+      toastIconColorClass = "tw:text-yellow-400";
+      break;
+    default:
+      break;
+  }
+
   return (
     <div
-      className={`-translate-x-1/2 tw:bg-black tw:rounded-lg tw:text-white tw:px-3 tw:py-2 tw:transition-all tw:duration-200 tw:ease-linear ${toast.additionalClass}`}
+      className={`pop-up -translate-x-1/2 tw:bg-hyperliquid-gray-200 tw:rounded-lg tw:text-white tw:px-3 tw:py-2 tw:transition-all tw:duration-200 tw:ease-linear ${toast.isExiting && "pop-out"}`}
     >
-      {toast.message} {toast.id}
+      <div className="tw:w-fit tw:flex tw:flex-row tw:items-center tw:justify-start tw:gap-2">
+        <i
+          className={`${toastIconClass} ${toastIconColorClass} tw:text-lg`}
+        ></i>
+
+        <div className="tw:text-sm tw:text-white">{toast.message}</div>
+      </div>
     </div>
   );
 };
