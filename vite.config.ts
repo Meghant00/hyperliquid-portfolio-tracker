db2b6a6c92000c3982361445bc6285b470e0f182
Meghant00 <dangolmeghant0@gmail.com>
2026-06-29 13:47:51 +0545

    :bug: : fix recharts loadsh error crashing app

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    target: "esnext",
+    commonjsOptions: {
+      transformMixedEsModules: true,
+      include: [/node_modules/],
+    },
+    rollupOptions: {
+      output: {
+        strictExecutionOrder: true,
+      },
+    },
+  },
 });
